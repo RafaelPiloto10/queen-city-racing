4ec99b393d7536a4cc9f586960ed08ae17b92e9f
Rafael <31188361+RafaelPiloto10@users.noreply.github.com>
2025-12-24 12:32:15 -0600

    add media links

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,26 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Queen City Racing",
   description: "Helping students go vroom one miata at a time",
+  openGraph: {
+    type: "website",
+    countryName: "US",
+    description:
+      "Queen City Racing | A Motorsport Team in the Collegiate Racing Series‌ ©",
+    determiner: "auto",
+    emails: "cincinnatiracing@gmail.com",
+    siteName: "Queen City Racing",
+    url: "queencityracing.vercel.app",
+    title: "Queen City Racing",
+    images: [
+      {
+        url: "https://queen-city-racing.vercel.app/_next/image?url=%2Fsponsor%2Fracetrack.png&w=3840&q=75",
+        width: 1200,
+        height: 630,
+        alt: "Queen City Racing Track",
+      },
+    ],
+    locale: "en_US",
+  },
 };
 
 export default function RootLayout({
